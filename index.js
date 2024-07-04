0385adfdce2114379c38d91dd9fb84d749157054
pranayjoshi <pranayjoshi4466@gmail.com>
2024-07-04 10:40:17 -0500

    initializedthe project and successfully integrated frameworks

---
+++ index.js
@@ -0,0 +1,35 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, './desktop/preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    mainWindow.loadURL('http://localhost:5173'); // The URL for Vite's dev server
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
